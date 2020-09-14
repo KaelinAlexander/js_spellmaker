722e7d156735c2d7fc643fa916025aba34fdf94a
KaelinAlexander <kaelin.alexander@gmail.com>
2020-09-14 14:05:58 -0600

    adds parts attr to join table

--- spellmaker_backend/db/migrate/20200914195452_create_spells_components.rb
+++ spellmaker_backend/db/migrate/20200914195452_create_spells_components.rb
@@ -1,6 +1,7 @@
 class CreateSpellsComponents < ActiveRecord::Migration[6.0]
   def change
     create_table :spells_components do |t|
+      t.integer :parts
       t.references :spell, null: false, foreign_key: true
       t.references :component, null: false, foreign_key: true
 
