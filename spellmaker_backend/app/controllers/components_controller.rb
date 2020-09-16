d43c0f33f826366c629ae6e6c4eebb45a6c1ae71
KaelinAlexander <kaelin.alexander@gmail.com>
2020-09-16 11:17:37 -0600

    adds initial backend create action

--- spellmaker_backend/app/controllers/components_controller.rb
+++ spellmaker_backend/app/controllers/components_controller.rb
@@ -1,12 +1,22 @@
 class ComponentsController < ApplicationController
 
     def index
-        render json: Component.all, include: { synonyms: {only: [:name] }, deities: {only: [:name] }, uses: {only: [:name]}}
+        render json: Component.all, include: { synonyms: {only: [:id, :name] }, deities: {only: [:id, :name] }, uses: {only: [:id, :name]}}
+    end
+
+    def create
+        byebug
+        @component = Component.new(component_params)
+        if @component.save
+            render json: @component, include: {synonyms: {only: [:id, :name]}, deities: {only: [:id, :name]}, uses: {only: [:id, :name]}}, status: :created
+        else
+            render json: @component.errors.full_messages, status: :unprocessable_entity
+        end 
     end
 
     private
     def component_params
-        params.require(:component).permit(:name, :latin, :planet, :element, :description)
+        params.require(:component).permit(:name, :latin, :planet, :element, :description, :toxic)
     end
 
 end
